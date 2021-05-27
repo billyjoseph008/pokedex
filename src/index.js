a4cee99774788bd7acae302c4f1922addf383e38
billyjoseph008 <billyjoseph08@hotmail.com>
2021-05-26 21:43:40 -0500

    Service Worker Fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom";
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import App from './App';
 import './global.css';
 import { I18nextProvider } from "react-i18next";
@@ -38,4 +38,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-serviceWorker.register();
+serviceWorkerRegistration.register();
